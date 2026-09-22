914d993fc58787ad7d5c73f19fde8b9c3df26929
Blind755 <83506802+Blind755@users.noreply.github.com>
2026-09-22 11:41:50 +0200

    Fixed Bugs👍

--- Kamo&Konquer/Assets/Script/UnitScript.cs
+++ Kamo&Konquer/Assets/Script/UnitScript.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class UnitScript : MonoBehaviour
 {
+    NavMeshAgent agent;
     
     void Start()
     {
         UnitSelectionManager.Instance.allUnitsList.Add(gameObject);
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+
 
     }
 
