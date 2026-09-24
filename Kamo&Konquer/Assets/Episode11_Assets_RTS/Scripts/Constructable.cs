c1aef784f1daa17e8940628ca7f500b275572f38
colinlanger-dev <colin.langer@gmx.de>
2026-09-24 12:09:54 +0200

    buildings damge bug to fix later

--- Kamo&Konquer/Assets/Episode11_Assets_RTS/Scripts/Constructable.cs
+++ Kamo&Konquer/Assets/Episode11_Assets_RTS/Scripts/Constructable.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,49 @@ using UnityEngine.AI;
 
 public class Constructable : MonoBehaviour
 {
+    [SerializeField] private Sprite spriteNormal;
+    [SerializeField] private Sprite spriteBeschaedigt;
+    private float constHealth;
+    public float constMaxHealth;
 
     NavMeshObstacle obstacle;
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+        constHealth = constMaxHealth;
+        UpdateHealthUI();
+    }
+
+    private void Update()
+    {
+        Console.WriteLine(constHealth);
+        
+    }
+
+    private void UpdateHealthUI()
+    {
+        Console.WriteLine(constHealth);
+        float prozent = constHealth / constMaxHealth;
+
+        if (prozent <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        else if (prozent < 0.5f)
+        {
+            sr.sprite = spriteBeschaedigt;
+        }
+
+        Console.WriteLine(prozent);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        constHealth -= damage;
+        UpdateHealthUI();
+    }
 
     public void ConstructableWasPlaced()
     {
